4c95156dd8a01e3a6df13555660531ffd83626ce
Dan Foley <dfoley@sleepbot.com>
2009-01-30 19:09:10 -0800

    oops, no doc plz

--- test/hpricot.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-['hpricot', 'builder'].each {|lib| gem lib }
-['hpricot', 'builder', 'test/unit'].each {|lib| require lib }
-require 'ruby-debug'
-
-debugger
-doc = Hpricot.XML '<test />'
-puts doc.inspect
